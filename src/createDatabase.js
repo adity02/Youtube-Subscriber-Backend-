658dc9234c7a76559d5a70311ec83063139aabe0
adity02 <adity05@gmail.com>
2023-11-27 17:22:41 +0530

    second commit

--- src/createDatabase.js
+++ src/createDatabase.js
@@ -2,36 +2,19 @@ const mongoose = require('mongoose')
 const subscriberModel = require('./models/subscribers')
 const data = require('./data')
 
-// config for env
 require("dotenv").config();
 
 // Connect to DATABASE
-//Local to URI
-// const DATABASE_URL = "mongodb://localhost/subscribers";
-
-
-// Cluster URI
-const DATABASE_URL =process.env.DATABASE_URI;
-
-//connect to MongoDB using Mongoose
-mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true, dbName:"subscribers" });
+const DATABASE_URL = process.env.DATABASE_URI;
+mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
-
-// handles the error
 db.on('error', (err) => console.log(err))
-//logs a success message
-db.once('open', () => console.log('Database created...', dbName))
+db.once('open', () => console.log('Database created...'))
 
-
-// Refresh all connections
 const refreshAll = async () => {
-    // console.log(deleting previous data)
     await subscriberModel.deleteMany({})
-
-    // console.log(inserting data)
+    // console.log(connection)
     await subscriberModel.insertMany(data)
-
-    // console.log(database disconnect)
     await mongoose.disconnect();
 }
 refreshAll()
